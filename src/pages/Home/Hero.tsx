2a0864f562edb92b1f204cd5628c30b26ef58e77
Joyjeet <joyxbhp6@gmail.com>
2024-07-15 16:42:18 +0530

    herp section added

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,12 +1,33 @@
+import Image from "next/image";
+import Idea from "../../assets/Idea.png";
+import Idea2 from "../../assets/Idea2.png";
+
 const Hero = () => {
   return (
     <div className="h-screen md:max-w-3xl lg:max-w-4xl md:mx-auto">
       <div className="">
-        <p className="absolute top-[30%] left-4 text-5xl font-btnfont font-semibold w-[40%] md:hidden">
-        Unleashing Brilliant Ideas for a Better Tomorrow
+        <div className="absolute top-[-1%] right-0 md:right-auto">
+          <Image src={Idea} alt="idea-png" />
+        </div>
+        <p className="absolute right-[25%] font-extrabold bottom-5 text-right w-[40%]  text-2xl font-btnfont hidden lg:block">
+          Innovation is born from the willingness to take risks and the courage
+          to dream big. Let your passion guide you to create something
+          extraordinary.
         </p>
-        <p className="absolute top-[40%] left-4 text-5xl font-btnfont font-semibold w-[60%] hidden md:block">
-        Empowering Your Journey with Innovative Ideas
+        <div className="absolute hidden right-0 bottom-0 md:block">
+          <Image
+            src={Idea2}
+            height={300}
+            width={300}
+            className=""
+            alt="idea-png"
+          />
+        </div>
+        <p className="absolute top-[30%] left-4 text-5xl font-btnfont font-semibold w-[40%] md:hidden">
+          Unleashing Brilliant Ideas for a Better Tomorrow
+        </p>
+        <p className="absolute top-[30%] left-4 text-5xl font-btnfont font-semibold w-[60%] hidden md:block">
+          Empowering Your Journey with Innovative Ideas
         </p>
       </div>
     </div>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,11 +1,16 @@
-
-import { Button } from "@/components/ui/button";
+"use client";
 import Hero from "./Hero";
-import Image from "next/image";
-import Idea from "../../assets/Idea.png"
-import Outofthebox from "../../assets/Outofthebox.png"
+import { useState } from "react";
+import Login from "../LoginRegsiter/Login";
+import Register from "../LoginRegsiter/Register";
 
 const HomePage = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleShowLogin = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
     <div className="md:max-w-3xl lg:max-w-6xl md:mx-auto ">
       <div className="relative ">
@@ -14,17 +19,12 @@ const HomePage = () => {
           <h1 className="font-logo font-extrabold text-2xl">Jixel</h1>
         </div>
         {/* Login/logout button */}
-        <div className="absolute right-4 top-4 md:flex font-btnfont">
-          <Button variant="outline">Login</Button>
-          <Button className="hidden md:block ml-2">Register</Button>
+        <div onClick={handleShowLogin} className={`${!showLogin ? `absolute right-4 top-4 md:flex font-btnfont z-[9999]` : `absolute right-4 top-4 md:flex font-btnfont z-[0] `}`}>
+          <Login />
+          <Register />
         </div>
       </div>
-      <div className="absolute top-[-1%] right-0 md:right-auto">
-        <Image src={Idea} alt="idea-png" />
-      </div>
-      <div className="absolute right-0 hidden bottom-0 md:block">
-        <Image src={Outofthebox} height={500} width={500} className="" alt="idea-png" />
-      </div>
+
       <div className="relative">
         {/* Hero Component */}
         <Hero />
