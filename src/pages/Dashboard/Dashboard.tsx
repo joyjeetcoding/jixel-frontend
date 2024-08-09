428d4e6ade514a88c36dfbd7316120c38999ea0d
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-08-10 03:02:08 +0530

    dashboard started

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,96 @@
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="absolute top-[20%]">
-      <div className="">Dashboard</div>
+    <div className="absolute top-[20%] mx-4 md:max-w-3xl lg:max-w-6xl md:mx-auto">
+      <div className="mb-5 md:mx-4">
+        <div className="grid md:grid-cols-3 place-content-between">
+          <div className="">
+            <h2 className="font-extrabold text-2xl text-center md:text-left">
+              AI founders play musical chairs
+            </h2>
+            <p className="mt-4 text-sm">Marina Temkin</p>
+            <p className="text-sm">3 hours ago</p>
+          </div>
+          <div className="hidden md:block">
+            <p className="px-4">Welcome to Startups Weekly — your weekly recap of everything you can’t miss from the world of startups. Want it in your inbox every Friday? Sign up here.</p>
+          </div>
+          <div className="hidden md:block">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-670880023.jpg?resize=1536,1086"
+              alt="imagefile"
+              className="w-[80%] float-right"
+            />
+          </div>
+        </div>
+        <Separator />
+      </div>
+      <div className="mb-5 md:mx-4">
+        <div className="grid md:grid-cols-3 place-content-between">
+          <div className="">
+            <h2 className="font-extrabold text-2xl text-center md:text-left">
+              AI founders play musical chairs
+            </h2>
+            <p className="mt-4 text-sm">Marina Temkin</p>
+            <p className="text-sm">3 hours ago</p>
+          </div>
+          <div className="hidden md:block">
+            <p className="px-4">Welcome to Startups Weekly — your weekly recap of everything you can’t miss from the world of startups. Want it in your inbox every Friday? Sign up here.</p>
+          </div>
+          <div className="hidden md:block">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-670880023.jpg?resize=1536,1086"
+              alt="imagefile"
+              className="w-[80%] float-right"
+            />
+          </div>
+        </div>
+        <Separator />
+      </div><div className="mb-5 md:mx-4">
+        <div className="grid md:grid-cols-3 place-content-between">
+          <div className="">
+            <h2 className="font-extrabold text-2xl text-center md:text-left">
+              AI founders play musical chairs
+            </h2>
+            <p className="mt-4 text-sm">Marina Temkin</p>
+            <p className="text-sm">3 hours ago</p>
+          </div>
+          <div className="hidden md:block">
+            <p className="px-4">Welcome to Startups Weekly — your weekly recap of everything you can’t miss from the world of startups. Want it in your inbox every Friday? Sign up here.</p>
+          </div>
+          <div className="hidden md:block">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-670880023.jpg?resize=1536,1086"
+              alt="imagefile"
+              className="w-[80%] float-right"
+            />
+          </div>
+        </div>
+        <Separator />
+      </div><div className="mb-5 md:mx-4">
+        <div className="grid md:grid-cols-3 place-content-between">
+          <div className="">
+            <h2 className="font-extrabold text-2xl text-center md:text-left">
+              AI founders play musical chairs
+            </h2>
+            <p className="mt-4 text-sm">Marina Temkin</p>
+            <p className="text-sm">3 hours ago</p>
+          </div>
+          <div className="hidden md:block">
+            <p className="px-4">Welcome to Startups Weekly — your weekly recap of everything you can’t miss from the world of startups. Want it in your inbox every Friday? Sign up here.</p>
+          </div>
+          <div className="hidden md:block">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2022/02/GettyImages-670880023.jpg?resize=1536,1086"
+              alt="imagefile"
+              className="w-[80%] float-right"
+            />
+          </div>
+        </div>
+        <Separator />
+      </div>
     </div>
   );
 };
